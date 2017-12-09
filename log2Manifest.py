3bfa43ceb522e75be68d82c4ceb72434913f0d3b
Izaac <izaac.kirscht@gmail.com>
2017-12-08 20:49:12 -0600

    log to manifest generator using json

---
+++ log2Manifest.py
@@ -0,0 +1,17 @@
+import json
+
+tempArray = []
+
+with open('secondLookup.json') as lookup:
+	jsonContents = json.load(lookup)
+
+with open('scanned.txt', 'r') as scanned:
+	for each in scanned:
+		tempArray.append(each.strip('\n'))
+
+print tempArray
+
+for scannedSKU in tempArray:
+	for lookupSKU in jsonContents:
+		if scannedSKU == lookupSKU['SKU']:
+			print scannedSKU, lookupSKU['SKU'], lookupSKU['Title']
